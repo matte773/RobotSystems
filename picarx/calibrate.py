3435a30fd0f01f8691d631ba2d162b96fbfc8990
matte773 <matte773@gmail.com>
2024-01-19 14:00:27 -0800

    Fixing PiCar_Improved File

--- picarx/calibrate.py
+++ picarx/calibrate.py
@@ -9,6 +9,7 @@ def calibrate_steering():
     initial_calibration_angle = 0
 
     # Command the system to drive forward at zero steering angle
+    picar.set_dir_servo_angle(initial_calibration_angle)
     picar.forward(75)  # Adjust speed as needed
     time.sleep(5)  # Adjust duration as needed
     picar.stop()
